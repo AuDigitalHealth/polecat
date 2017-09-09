dfc9af903d63d9abb8eaeef7ef5059c0ebe55f8c
John Grimes <john@grimes.id.au>
2017-09-09 21:53:33 +1000

    Link MPPs to MPUUs and remove linkages up through TPUU

--- src/fhir/translations.js
+++ src/fhir/translations.js
@@ -5,11 +5,12 @@ import {
 } from './medication.js'
 
 export const translateToAmt = concepts => {
-  const tpsResolved = resolveTP(concepts)
-  return resolveTPUU(tpsResolved)
+  const tpsResolved = resolveTp(concepts)
+  const tpuusResolved = resolveTpuu(tpsResolved)
+  return resolveMpuu(tpuusResolved)
 }
 
-const resolveTP = concepts => {
+const resolveTp = concepts => {
   let newRelationships = concepts.relationships
   const ctppTps = concepts.relationships.filter(r =>
     relationshipMatchesTypes(r, 'CTPP', 'TP', concepts.concepts)
@@ -45,7 +46,7 @@ const resolveTP = concepts => {
   return { ...concepts, relationships: newRelationships }
 }
 
-const resolveTPUU = concepts => {
+const resolveTpuu = concepts => {
   let newRelationships = concepts.relationships
   const ctppTpuus = concepts.relationships.filter(r =>
     relationshipMatchesTypes(r, 'CTPP', 'TPUU', concepts.concepts)
@@ -75,6 +76,41 @@ const resolveTPUU = concepts => {
       }
       newRelationships = newRelationships.filter(
         r => !relationshipMatchesIds(r, ctpp, tpuu)
+      )
+    }
+  })
+  return { ...concepts, relationships: newRelationships }
+}
+
+const resolveMpuu = concepts => {
+  let newRelationships = concepts.relationships
+  const tppTpuus = concepts.relationships.filter(r =>
+    relationshipMatchesTypes(r, 'TPP', 'TPUU', concepts.concepts)
+  )
+  tppTpuus.forEach(tppTpuu => {
+    const tpuu = tppTpuu.target
+    const tpp = tppTpuu.source
+    const tpuuMpuu = concepts.relationships.find(r =>
+      relationshipMatchesIdAndType(r, tpuu, 'MPUU', concepts.concepts)
+    )
+    const tppMpp = concepts.relationships.find(r =>
+      relationshipMatchesIdAndType(r, tpp, 'MPP', concepts.concepts)
+    )
+    if (tpuuMpuu && tppMpp) {
+      const mpp = tppMpp.target
+      const mpuu = tpuuMpuu.target
+      newRelationships = newRelationships.concat([
+        {
+          source: mpp,
+          target: mpuu,
+          type: relationshipTypeFor(
+            fhirMedicationTypeFor('MPP'),
+            fhirMedicationTypeFor('MPUU')
+          ),
+        },
+      ])
+      newRelationships = newRelationships.filter(
+        r => !relationshipMatchesIds(r, tpuu, mpuu, concepts.concepts)
       )
     }
   })
