af088fe875c4363d4c9cf5b4b2ffe735ee0471a0
John Grimes <john@grimes.id.au>
2017-09-15 13:41:41 +1000

    Add association link types

--- src/graph/links.js
+++ src/graph/links.js
@@ -4,27 +4,23 @@ export const curveForLink = (link, i, options) => {
   let mergedOptions = calculateBearings(link, options)
   mergedOptions = calculateLinkEndings(link, mergedOptions)
   mergedOptions = calculateControlPoints(mergedOptions)
-  mergedOptions = calculateArrowPoints(mergedOptions)
-  const {
-    startX,
-    startY,
-    endX,
-    endY,
-    cp1x,
-    cp1y,
-    cp2x,
-    cp2y,
-    arrowPoints,
-  } = mergedOptions
-  const linkCurve = `M ${startX} ${startY} C ${cp1x} ${cp1y} ${cp2x} ${cp2y} ${endX} ${endY}`
-  const arrow =
-    `M ${arrowPoints[0][0]} ${arrowPoints[0][1]} ` +
-    arrowPoints.reduce((a, p) => a + `L ${p[0]} ${p[1]} `, '') +
-    `L ${arrowPoints[0][0]} ${arrowPoints[0][1]}`
+  const { startX, startY, endX, endY, cp1x, cp1y, cp2x, cp2y } = mergedOptions
+  const linkPath = `M ${startX} ${startY} C ${cp1x} ${cp1y} ${cp2x} ${cp2y} ${endX} ${endY}`
+  let arrow = null
+  if (link.type !== 'unknown') {
+    const { arrowPoints } = calculateArrowPoints({
+      ...mergedOptions,
+      linkType: link.type,
+    })
+    const arrowPath =
+      `M ${arrowPoints[0][0]} ${arrowPoints[0][1]} ` +
+      arrowPoints.reduce((a, p) => a + `L ${p[0]} ${p[1]} `, '').trim()
+    arrow = <path className='arrow' d={arrowPath} />
+  }
   return (
-    <g key={i} className='relationship'>
-      <path className='link' d={linkCurve} />
-      <path className='arrow' d={arrow} />
+    <g key={i} className={`relationship relationship-${link.type}`}>
+      <path className='link' d={linkPath} />
+      {arrow}
     </g>
   )
 }
@@ -171,32 +167,45 @@ const calculateArrowPoints = options => {
       arrowSize,
       endX,
       endY,
+      linkType,
     } = options,
     halfArrowSize = arrowSize / 2
-  let point1, point2, point3
+  const points = []
   switch (true) {
     case bearing >= northEast || bearing < southEast:
-      point1 = [ endX, endY ]
-      point2 = [ endX - arrowSize, endY + halfArrowSize ]
-      point3 = [ endX - arrowSize, endY - halfArrowSize ]
+      points.push([ endX - arrowSize, endY + halfArrowSize ])
+      points.push([ endX, endY ])
+      points.push([ endX - arrowSize, endY - halfArrowSize ])
+      if (linkType === 'is-a') {
+        points.push([ endX - arrowSize, endY + halfArrowSize ])
+      }
       break
     case bearing >= southEast && bearing < southWest:
-      point1 = [ endX, endY ]
-      point2 = [ endX - halfArrowSize, endY - Math.sqrt(3) / 2 * arrowSize ]
-      point3 = [ endX + halfArrowSize, endY - Math.sqrt(3) / 2 * arrowSize ]
+      points.push([ endX - halfArrowSize, endY - Math.sqrt(3) / 2 * arrowSize ])
+      points.push([ endX, endY ])
+      points.push([ endX + halfArrowSize, endY - Math.sqrt(3) / 2 * arrowSize ])
+      if (linkType === 'is-a') {
+        points.push([ endX - halfArrowSize, endY - Math.sqrt(3) / 2 * arrowSize ])
+      }
       break
     case bearing >= southWest && bearing < northWest:
-      point1 = [ endX, endY ]
-      point2 = [ endX + arrowSize, endY + halfArrowSize ]
-      point3 = [ endX + arrowSize, endY - halfArrowSize ]
+      points.push([ endX + arrowSize, endY + halfArrowSize ])
+      points.push([ endX, endY ])
+      points.push([ endX + arrowSize, endY - halfArrowSize ])
+      if (linkType === 'is-a') {
+        points.push([ endX + arrowSize, endY + halfArrowSize ])
+      }
       break
     case bearing >= northWest && bearing < northEast:
-      point1 = [ endX, endY ]
-      point2 = [ endX - halfArrowSize, endY + Math.sqrt(3) / 2 * arrowSize ]
-      point3 = [ endX + halfArrowSize, endY + Math.sqrt(3) / 2 * arrowSize ]
+      points.push([ endX - halfArrowSize, endY + Math.sqrt(3) / 2 * arrowSize ])
+      points.push([ endX, endY ])
+      points.push([ endX + halfArrowSize, endY + Math.sqrt(3) / 2 * arrowSize ])
+      if (linkType === 'is-a') {
+        points.push([ endX - halfArrowSize, endY + Math.sqrt(3) / 2 * arrowSize ])
+      }
       break
     default:
       throw new Error(`Unexpected bearing: ${bearing}`)
   }
-  return { ...options, arrowPoints: [ point1, point2, point3 ] }
+  return { ...options, arrowPoints: points }
 }
