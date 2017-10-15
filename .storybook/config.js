b43cd01582325a3ae8c4005939d071ebc777807d
John Grimes <john@grimes.id.au>
2017-10-16 05:49:52 +1000

    Add storybook options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
+import React, { Component } from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import StoryRouter from 'storybook-router'
 import { withKnobsOptions } from '@storybook/addon-knobs'
 import { withSmartKnobs } from 'storybook-addon-smart-knobs'
+import { setOptions } from '@storybook/addon-options'
 
 addDecorator(withSmartKnobs)
 addDecorator(withKnobsOptions({ debounce: { wait: 350 } }))
@@ -13,4 +15,5 @@ function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
+setOptions({ name: 'Polecat' })
 configure(loadStories, module)
