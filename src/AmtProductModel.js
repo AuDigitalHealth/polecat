f8f07e8a4b3d22a3b0b51b83927d5cb9e6a50cf4
John Grimes <john@grimes.id.au>
2017-10-24 14:49:33 +1000

    Only fix focused node if there are more than two nodes

--- src/AmtProductModel.js
+++ src/AmtProductModel.js
@@ -37,7 +37,6 @@ class AmtProductModel extends Component {
       PropTypes.shape({ source: PropTypes.string, target: PropTypes.string })
     ),
     attraction: PropTypes.number,
-    collideRadius: PropTypes.number,
     linkDistance: PropTypes.number,
     alpha: PropTypes.number,
     alphaDecay: PropTypes.number,
@@ -47,8 +46,14 @@ class AmtProductModel extends Component {
     conceptGroupHeight: PropTypes.number,
     linkCurviness: PropTypes.number,
     arrowSize: PropTypes.number,
-    collideRadiusThreshold: PropTypes.number,
+    // Specifies the ratio between concept radius and collide radius.
     collideRadiusRatio: PropTypes.number,
+    // Specifies a threshold over which an additional multiplier is applied to
+    // collide threshold.
+    collideRadiusThreshold: PropTypes.number,
+    // Increases collide radius by an additional multiplier for each concept
+    // over the `collideRadiusThreshold`.
+    collideRadiusMultiplier: PropTypes.number,
     viewport: PropTypes.shape({
       width: PropTypes.number,
       height: PropTypes.number,
@@ -56,7 +61,6 @@ class AmtProductModel extends Component {
   }
   static defaultProps = {
     attraction: -1000,
-    collideRadius: 110,
     linkDistance: 220,
     alpha: 1.5,
     alphaDecay: 0.1,
@@ -66,8 +70,9 @@ class AmtProductModel extends Component {
     conceptGroupHeight: 47,
     linkCurviness: 0.4,
     arrowSize: 10,
-    collideRadiusThreshold: 10,
     collideRadiusRatio: 1.5,
+    collideRadiusThreshold: 10,
+    collideRadiusMultiplier: 1.5,
   }
 
   constructor(props) {
@@ -81,53 +86,22 @@ class AmtProductModel extends Component {
     this.renderRelationships = this.renderRelationships.bind(this)
   }
 
-  startOrUpdateSimulation(
-    nodes,
-    links,
-    attraction,
-    collideRadius,
-    linkDistance,
-    alpha,
-    alphaDecay,
-    collideRadiusThreshold,
-    collideRadiusRatio,
-    centerX,
-    centerY
-  ) {
+  startOrUpdateSimulation(nodes, links, options) {
     const model = this
     let newNodes = null
-    const { viewport } = this.props
+    const {
+      attraction,
+      linkDistance,
+      alpha,
+      alphaDecay,
+      centerX,
+      centerY,
+      viewport,
+    } = options
     // If the simulation is already running, merge new nodes in instead of
     // replacing them.
     if (model.simulation) {
-      let oldNodes = model.simulation.nodes()
-      // Remove focus and fixing from any existing nodes.
-      oldNodes = oldNodes.map(node => omit(node, 'focused', 'fx', 'fy'))
-      // Merge new nodes with old nodes.
-      newNodes = [ oldNodes, nodes ].reduce(mergeConcepts, [])
-      // Remove any nodes that are not present in the new set of nodes.
-      newNodes = newNodes.filter(node =>
-        nodes
-          .map(n => AmtProductModel.idForNode(n))
-          .includes(AmtProductModel.idForNode(node))
-      )
-      // Remove any nodes that are no longer the subject of a link (except the
-      // focused node).
-      newNodes = newNodes.filter(
-        node =>
-          node.focused ||
-          links
-            .reduce((acc, link) => acc.concat([ link.source, link.target ]), [])
-            .includes(AmtProductModel.idForNode(node))
-      )
-      // Fix the position of the focused node.
-      newNodes = newNodes.map(node => {
-        if (node.focused) {
-          node.fx = node.x = centerX || viewport.width / 2
-          node.fy = node.y = centerY || viewport.height / 2
-        }
-        return node
-      })
+      newNodes = this.startSimulation(nodes, links, options)
     } else newNodes = nodes
     model.simulation = (model.simulation || d3.forceSimulation()).nodes(
       newNodes
@@ -139,11 +113,8 @@ class AmtProductModel extends Component {
     model.forceManyBody = (model.forceManyBody || d3.forceManyBody()).strength(
       attraction
     )
-    model.forceCollide = (model.forceCollide || d3.forceCollide()).radius(
-      newNodes.length > collideRadiusThreshold
-        ? collideRadius +
-          collideRadiusRatio * (newNodes.length - collideRadiusThreshold)
-        : collideRadius
+    model.forceCollide = (model.forceCollide || d3.forceCollide()).radius(d =>
+      this.calculateCollideRadius(newNodes, d, options)
     )
     model.forceCenter = (model.forceCenter || d3.forceCenter())
       .x(centerX || viewport.width / 2)
@@ -164,11 +135,46 @@ class AmtProductModel extends Component {
       .restart()
   }
 
+  startSimulation(nodes, links, options) {
+    const model = this
+    const { centerX, centerY, viewport } = options
+    let oldNodes = model.simulation.nodes()
+    // Remove focus and fixing from any existing nodes.
+    oldNodes = oldNodes.map(node => omit(node, 'focused', 'fx', 'fy'))
+    // Merge new nodes with old nodes.
+    let newNodes = [ oldNodes, nodes ].reduce(mergeConcepts, [])
+    // Remove any nodes that are not present in the new set of nodes.
+    newNodes = newNodes.filter(node =>
+      nodes
+        .map(n => AmtProductModel.idForNode(n))
+        .includes(AmtProductModel.idForNode(node))
+    )
+    // Remove any nodes that are no longer the subject of a link (except the
+    // focused node).
+    newNodes = newNodes.filter(
+      node =>
+        node.focused ||
+        links
+          .reduce((acc, link) => acc.concat([ link.source, link.target ]), [])
+          .includes(AmtProductModel.idForNode(node))
+    )
+    // Fix the position of the focused node, if there are more than two nodes.
+    return newNodes.length > 2
+      ? newNodes.map(node => {
+        if (node.focused) {
+          node.fx = node.x = centerX || viewport.width / 2
+          node.fy = node.y = centerY || viewport.height / 2
+        }
+        return node
+      })
+      : newNodes
+  }
+
   stopSimulation() {
     if (this.simulation) this.simulation.stop()
   }
 
-  moveSimulationCenter(deltaX, deltaY, newX, newY) {
+  moveSimulationCenter(deltaX, deltaY, newX, newY, options) {
     const model = this
     const forceCenter = model.simulation.force('center')
     const nodes = model.simulation.nodes().map(node => ({
@@ -178,34 +184,41 @@ class AmtProductModel extends Component {
       fx: node.fx ? node.fx + deltaX : null,
       fy: node.fy ? node.fy + deltaY : null,
     }))
-    const {
-      links,
-      attraction,
-      collideRadius,
-      linkDistance,
-      alphaDecay,
-      collideRadiusThreshold,
-      collideRadiusRatio,
-    } = this.props
     const { relationships: translatedLinks } = translateToAmt({
       concepts: nodes,
-      relationships: links,
+      relationships: options.links,
     })
     const centerX = deltaX !== null ? forceCenter.x() + deltaX : newX
     const centerY = deltaY !== null ? forceCenter.y() + deltaY : newY
-    this.startOrUpdateSimulation(
-      nodes,
-      translatedLinks,
-      attraction,
-      collideRadius,
-      linkDistance,
-      model.simulation.alpha(),
-      alphaDecay,
+    this.startOrUpdateSimulation(nodes, translatedLinks, {
+      ...options,
+      alpha: model.simulation.alpha(),
+      centerX,
+      centerY,
+    })
+  }
+
+  calculateCollideRadius(nodes, node, options) {
+    const {
+      conceptGroupHeight,
+      conceptGroupWidth,
+      conceptHeight,
+      conceptWidth,
       collideRadiusThreshold,
       collideRadiusRatio,
-      centerX,
-      centerY
-    )
+      collideRadiusMultiplier,
+    } = options
+    // Sets a different collide radius for concepts and concept groups.
+    const conceptRadius = codingToGroupCode(node.coding)
+      ? conceptGroupHeight /
+        2 /
+        Math.cos(Math.atan(conceptGroupHeight / conceptGroupWidth))
+      : conceptHeight / 2 / Math.cos(Math.atan(conceptHeight / conceptWidth))
+    // Applies the ratio and multiplier options.
+    return nodes.length > collideRadiusThreshold
+      ? conceptRadius * collideRadiusRatio +
+          collideRadiusMultiplier * (nodes.length - collideRadiusThreshold)
+      : conceptRadius * collideRadiusRatio
   }
 
   handleMouseUp(event) {
@@ -223,7 +236,13 @@ class AmtProductModel extends Component {
       const clientX = event.clientX
       const clientY = event.clientY
       if (lastDragX && lastDragY) {
-        this.moveSimulationCenter(clientX - lastDragX, clientY - lastDragY)
+        this.moveSimulationCenter(
+          clientX - lastDragX,
+          clientY - lastDragY,
+          null,
+          null,
+          this.props
+        )
         this.setState(() => ({
           lastDragX: clientX,
           lastDragY: clientY,
@@ -238,7 +257,13 @@ class AmtProductModel extends Component {
   }
 
   handleWheel(event) {
-    this.moveSimulationCenter(event.deltaX * -1, event.deltaY * -1)
+    this.moveSimulationCenter(
+      event.deltaX * -1,
+      event.deltaY * -1,
+      null,
+      null,
+      this.props
+    )
     event.preventDefault()
   }
 
@@ -247,38 +272,19 @@ class AmtProductModel extends Component {
       null,
       null,
       this.props.viewport.width / 2,
-      this.props.viewport.height / 2
+      this.props.viewport.height / 2,
+      this.props
     )
   }
 
   componentDidMount() {
-    const {
-      nodes,
-      links,
-      attraction,
-      collideRadius,
-      linkDistance,
-      alpha,
-      alphaDecay,
-      collideRadiusThreshold,
-      collideRadiusRatio,
-    } = this.props
+    const { nodes, links } = this.props
     if (nodes && links) {
       const { relationships: translatedLinks } = translateToAmt({
         concepts: nodes,
         relationships: links,
       })
-      this.startOrUpdateSimulation(
-        nodes,
-        translatedLinks,
-        attraction,
-        collideRadius,
-        linkDistance,
-        alpha,
-        alphaDecay,
-        collideRadiusThreshold,
-        collideRadiusRatio
-      )
+      this.startOrUpdateSimulation(nodes, translatedLinks, this.props)
     }
   }
 
@@ -293,34 +299,15 @@ class AmtProductModel extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const {
-      attraction,
-      collideRadius,
-      linkDistance,
-      alpha,
-      alphaDecay,
-      collideRadiusThreshold,
-      collideRadiusRatio,
-      viewport: { width, height },
-    } = nextProps
     if (!(nextProps.nodes && nextProps.links)) return
     const { concepts: nodes, relationships: links } = translateToAmt({
       concepts: nextProps.nodes,
       relationships: nextProps.links,
     })
-    this.startOrUpdateSimulation(
-      nodes,
-      links,
-      attraction,
-      collideRadius,
-      linkDistance,
-      alpha,
-      alphaDecay,
-      collideRadiusThreshold,
-      collideRadiusRatio
-    )
+    this.startOrUpdateSimulation(nodes, links, nextProps)
     if (!isEqual(this.props.viewport, nextProps.viewport)) {
-      this.moveSimulationCenter(null, null, width / 2, height / 2)
+      const { viewport: { width, height } } = nextProps
+      this.moveSimulationCenter(null, null, width / 2, height / 2, nextProps)
     }
   }
 
@@ -406,26 +393,9 @@ class AmtProductModel extends Component {
   }
 
   renderRelationships() {
-    const {
-        conceptWidth,
-        conceptHeight,
-        conceptGroupWidth,
-        conceptGroupHeight,
-        linkCurviness,
-        arrowSize,
-      } = this.props,
-      { links } = this.state
+    const { links } = this.state
     return links
-      ? links.map((link, i) =>
-        curveForLink(link, i, {
-          conceptWidth,
-          conceptHeight,
-          conceptGroupWidth,
-          conceptGroupHeight,
-          linkCurviness,
-          arrowSize,
-        })
-      )
+      ? links.map((link, i) => curveForLink(link, i, this.props))
       : []
   }
 
--- src/AmtProductModel.stories.js
+++ src/AmtProductModel.stories.js
@@ -8,6 +8,47 @@ const { nodes: nodes1, links: links1 } = nodesLinks1
 const width = 800
 const height = 800
 
-storiesOf('AmtProductModel', module).add('With nodes and links', () => (
-  <AmtProductModel nodes={nodes1} links={links1} viewport={{ width, height }} />
-))
+const twoNodes = {
+  nodes: [
+    {
+      coding: [
+        {
+          system: 'http://snomed.info/sct',
+          code: '21415011000036100',
+          display: 'amoxicillin',
+        },
+      ],
+      type: 'UPD',
+    },
+    {
+      type: 'UPD',
+      coding: [
+        {
+          system: 'http://snomed.info/sct',
+          code: '21360011000036101',
+          display: 'amoxicillin + clavulanic acid',
+        },
+      ],
+      focused: true,
+    },
+  ],
+  links: [
+    { source: '21360011000036101', target: '21415011000036100', type: 'is-a' },
+  ],
+}
+
+storiesOf('AmtProductModel', module)
+  .add('With nodes and links', () => (
+    <AmtProductModel
+      nodes={nodes1}
+      links={links1}
+      viewport={{ width, height }}
+    />
+  ))
+  .add('Two nodes', () => (
+    <AmtProductModel
+      nodes={twoNodes.nodes}
+      links={twoNodes.links}
+      viewport={{ width, height }}
+    />
+  ))
