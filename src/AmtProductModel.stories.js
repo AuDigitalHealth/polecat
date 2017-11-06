30f8c5e1eaae8f07c8092d38e2cf397ef4e35793
John Grimes <john@grimes.id.au>
2017-11-07 08:10:55 +1000

    Add stories covering different link orientations

--- src/AmtProductModel.stories.js
+++ src/AmtProductModel.stories.js
@@ -33,22 +33,52 @@ const twoNodes = {
     },
   ],
   links: [
-    { source: '21360011000036101', target: '21415011000036100', type: 'is-a' },
+    { source: '21415011000036100', target: '21360011000036101', type: 'is-a' },
   ],
 }
 
-storiesOf('AmtProductModel', module)
-  .add('With nodes and links', () => (
-    <AmtProductModel
-      nodes={nodes1}
-      links={links1}
-      viewport={{ width, height }}
-    />
-  ))
-  .add('Two nodes', () => (
-    <AmtProductModel
-      nodes={twoNodes.nodes}
-      links={twoNodes.links}
-      viewport={{ width, height }}
-    />
-  ))
+const fixedPos = {
+  E: [ [ 100, 100 ], [ 400, 100 ] ],
+  ESE: [ [ 100, 100 ], [ 400, 194.41696971 ] ],
+  SE: [ [ 100, 100 ], [ 400, 309.6385542 ] ],
+  SSE: [ [ 100, 100 ], [ 400, 389.6986192 ] ],
+  S: [ [ 100, 100 ], [ 100, 300 ] ],
+  SSW: [ [ 400, 100 ], [ 100, 389.6986192 ] ],
+  SW: [ [ 400, 100 ], [ 100, 309.6385542 ] ],
+  WSW: [ [ 400, 100 ], [ 100, 194.41696971 ] ],
+  W: [ [ 400, 100 ], [ 100, 100 ] ],
+  WNW: [ [ 400, 194.41696971 ], [ 100, 100 ] ],
+  NW: [ [ 400, 309.6385542 ], [ 100, 100 ] ],
+  NNW: [ [ 400, 389.6986192 ], [ 100, 100 ] ],
+  N: [ [ 100, 300 ], [ 100, 100 ] ],
+  NNE: [ [ 100, 389.6986192 ], [ 400, 100 ] ],
+  NE: [ [ 100, 309.6385542 ], [ 400, 100 ] ],
+  ENE: [ [ 100, 194.41696971 ], [ 400, 100 ] ],
+  Close: [ [ 100, 100 ], [ 280, 232.7710843 ] ],
+}
+
+const addFixedPos = (nodes, fixedPos) =>
+  nodes.map((node, i) => ({
+    ...{ fx: fixedPos[i][0], fy: fixedPos[i][1] },
+    ...node,
+  }))
+
+const stories = storiesOf(
+  'AmtProductModel',
+  module
+).add('With nodes and links', () => (
+  <AmtProductModel nodes={nodes1} links={links1} viewport={{ width, height }} />
+))
+
+for (const pos in fixedPos) {
+  stories.add(`${pos} link`, () => {
+    const nodes = addFixedPos(twoNodes.nodes, fixedPos[pos])
+    return (
+      <AmtProductModel
+        nodes={nodes}
+        links={twoNodes.links}
+        viewport={{ width, height }}
+      />
+    )
+  })
+}
