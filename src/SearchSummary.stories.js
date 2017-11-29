03f38b028713fbf19089e6787de65710ebc64d1a
John Grimes <john@grimes.id.au>
2017-11-30 07:47:19 +1000

    Add next and previous links to search summary stories

--- src/SearchSummary.stories.js
+++ src/SearchSummary.stories.js
@@ -6,3 +6,16 @@ import SearchSummary from './SearchSummary.js'
 storiesOf('SearchSummary', module)
   .add('With total results', () => <SearchSummary totalResults={5345} />)
   .add('With no total results', () => <SearchSummary />)
+  .add('With next link', () => (
+    <SearchSummary totalResults={350} nextLink='something' />
+  ))
+  .add('With previous link', () => (
+    <SearchSummary totalResults={350} previousLink='something' />
+  ))
+  .add('With next and previous links', () => (
+    <SearchSummary
+      totalResults={350}
+      previousLink='something'
+      nextLink='something-else'
+    />
+  ))
