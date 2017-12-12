c34aeda4c9035500abb58b4d03f143865491f83f
John Grimes <john@grimes.id.au>
2017-12-12 20:57:17 +1000

    Add sorry message for users of IE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import http from 'axios'
+import bowser from 'bowser'
 
 import Router from './Router'
 import addResourceHints from './resourceHints.js'
@@ -8,15 +9,24 @@ import registerServiceWorker from './registerServiceWorker'
 
 import './css/index.css'
 
-http
-  .get('/config.json')
-  .then(response => {
-    const config = response.data
-    ReactDOM.render(<Router config={config} />, document.getElementById('root'))
-  })
-  .catch(error => {
-    console.error(error)
-    ReactDOM.render(<Router />, document.getElementById('root'))
-  })
-addResourceHints()
-registerServiceWorker()
+if (bowser.msie && bowser.version <= 11) {
+  document.write(
+    '<div class="incompatible-browser"><p>&#x1f625&#x1f625&#x1f625</p><p>You appear to be running a very old web browser, which does not have the necessary features to run this application...</p><p>&#x1f625&#x1f625&#x1f625</p></div>'
+  )
+} else {
+  http
+    .get('/config.json')
+    .then(response => {
+      const config = response.data
+      ReactDOM.render(
+        <Router config={config} />,
+        document.getElementById('root')
+      )
+    })
+    .catch(error => {
+      console.error(error)
+      ReactDOM.render(<Router />, document.getElementById('root'))
+    })
+  addResourceHints()
+  registerServiceWorker()
+}
