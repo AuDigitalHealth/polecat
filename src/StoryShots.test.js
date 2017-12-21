a1729152073f24d5af00fed53a32858255873b4c
John Grimes <john@grimes.id.au>
2017-12-21 20:46:08 +1000

    Fix snapshot testing

--- src/StoryShots.test.js
+++ src/StoryShots.test.js
@@ -1,15 +1,20 @@
 import initStoryshots from '@storybook/addon-storyshots'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import WrapStory from '@storybook/addon-knobs/dist/react/WrapStory'
+import { Route } from 'react-router'
 
 const shallowSnapshot = ({ story, context }) => {
-  // This is required to exclude the story router from the rendering of the
-  // snapshot.
-  const element = story.render(context).props.story()
-  let result = shallow(element)
-  // This moves us one more component down the tree in cases where we use an
-  // additional `host` decorator.
-  if (result.prop('story')) result = shallow(result.prop('story')())
+  const element = story
+    .render(context)
+    .props.story()
+    .props.storyFn()
+  const options = { disableLifecycleMethods: true }
+  let result = shallow(element, options)
+  const wrapStory = result.find(WrapStory)
+  if (wrapStory.exists()) result = shallow(wrapStory.prop('storyFn')(), options)
+  const route = result.find(Route)
+  if (route.exists()) result = shallow(route.prop('render')(), options)
   expect(result).toMatchSnapshot()
 }
 
