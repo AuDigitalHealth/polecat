87a5f06e4f0059ec9feaabc6997a2e6f6aaa74b5
John Grimes <john@grimes.id.au>
2018-01-08 11:12:03 +1000

    Update pipelines configuration for deployment to Medserve server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh $DEPLOY_USER@$SERVER_HOSTNAME "cd $DEPLOY_DIRECTORY && docker-compose pull johngrimes/polecat && docker-compose up -d"
+ssh $DEPLOY_USER@$SERVER_HOSTNAME "cd $DEPLOY_DIRECTORY && docker login -u $DOCKER_USER -p $DOCKER_PASSWORD && docker-compose pull polecat && docker logout && docker-compose up -d"
--- scripts/pushDockerImage.sh
+++ scripts/pushDockerImage.sh
@@ -2,3 +2,4 @@
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push $DOCKER_IMAGE
+docker logout
