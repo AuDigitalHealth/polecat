0936228b8010235e889266965fa5e5dad5bf5e77
John Grimes <john@grimes.id.au>
2018-01-09 21:34:29 +1000

    Fix bug with trying to parse non-integer SCTID candidate
    Resolves #9.

--- src/snomed/sctid.js
+++ src/snomed/sctid.js
@@ -1,12 +1,8 @@
 import verhoeff from 'verhoeff'
 
 export const isValidSctid = sctid => {
-  let int
-  try {
-    int = parseInt(sctid, 10)
-  } catch (e) {
-    return false
-  }
+  const int = parseInt(sctid, 10)
+  if (!int) return false
   if (int <= 10 ** 5 || int > 10 ** 18) return false
   const partitionId = sctid.slice(-3, -1)
   if (!['00', '01', '02', '10', '11', '12'].includes(partitionId)) return false
