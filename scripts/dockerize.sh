21f1328be96cc123dc6787817ad7afbb15f13760
John Grimes <john@grimes.id.au>
2018-01-11 08:39:55 +1000

    Remove unnecessary commands from dockerize script

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-cp -R build release
-find release -name "*.map" -exec rm {} \;
-rm -f release/config.json
 docker build -t $DOCKER_IMAGE --build-arg version=$(git rev-parse HEAD) .
-rm -rf release
