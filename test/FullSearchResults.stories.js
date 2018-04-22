dd03e295ecd13b6927b953551f8bee6b32badc31
John Grimes <john@grimes.id.au>
2018-04-22 11:07:32 +1000

    Upgrade dependencies

--- test/FullSearchResults.stories.js
+++ test/FullSearchResults.stories.js
@@ -6,12 +6,18 @@ import { results1 } from './QuickSearchResults.stories.js'
 
 storiesOf('FullSearchResults', module)
   .add('With full range of types', () => (
-    <FullSearchResults results={results1} />
+    <FullSearchResults results={results1} onRequireMoreResults={jest.fn()} />
   ))
   .add('With a query and empty results', () => (
-    <FullSearchResults query="dog" results={[]} />
+    <FullSearchResults
+      query="dog"
+      results={[]}
+      onRequireMoreResults={jest.fn()}
+    />
   ))
   .add('With no query and empty results', () => (
-    <FullSearchResults results={[]} />
+    <FullSearchResults results={[]} onRequireMoreResults={jest.fn()} />
   ))
-  .add('With no query and no results', () => <FullSearchResults />)
+  .add('With no query and no results', () => (
+    <FullSearchResults onRequireMoreResults={jest.fn()} />
+  ))
