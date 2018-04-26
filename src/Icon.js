14e556224ac9cef1f32f67475b04f89f5dd11adc
John Grimes <john@grimes.id.au>
2018-04-26 12:24:06 +1000

    Fix flickering and patchy ability to click SVG icons in Edge
    Resolves #36.

--- src/Icon.js
+++ src/Icon.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 
 import icons from './img/icons.svg'
 
+import './css/Icon.css'
+
 class Icon extends Component {
   static propTypes = {
     type: PropTypes.string.isRequired,
@@ -27,8 +29,12 @@ class Icon extends Component {
     this.setState(() => ({ mouse: 'hover' }))
   }
 
-  handleMouseLeave() {
-    this.setState(() => ({ mouse: 'none' }))
+  // This is necessary to prevent flickering of the active state as you move
+  // over the icon in Edge.
+  // See: https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7787318/
+  handleMouseLeave(event) {
+    if (event.target.className === 'icon')
+      this.setState(() => ({ mouse: 'none' }))
   }
 
   handleClick(event) {
@@ -41,23 +47,23 @@ class Icon extends Component {
     const { type, hoverType, width, height, alt, title, className } = this.props
     const { mouse } = this.state
     return (
-      <svg
+      <div
         className={className ? `icon ${className}` : 'icon'}
-        width={width}
-        height={height}
-        viewBox="0 0 100 100"
+        style={{ width, height }}
         onMouseOver={this.handleMouseOver}
         onMouseLeave={this.handleMouseLeave}
         onClick={this.handleClick}
       >
-        <title>{title}</title>
-        <desc>{alt}</desc>
-        <use
-          xlinkHref={`${icons}#${
-            hoverType && mouse === 'hover' ? hoverType : type
-          }`}
-        />
-      </svg>
+        <svg width={width} height={height} viewBox="0 0 100 100">
+          <title>{title}</title>
+          <desc>{alt}</desc>
+          <use
+            xlinkHref={`${icons}#${
+              hoverType && mouse === 'hover' ? hoverType : type
+            }`}
+          />
+        </svg>
+      </div>
     )
   }
 }
