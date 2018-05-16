4b5b86536c20e67cd85e45192878ee12c040f86d
John Grimes <john@grimes.id.au>
2018-05-17 06:14:06 +1000

    Make record release script tolerant of not being able to set commits

--- scripts/recordRelease.sh
+++ scripts/recordRelease.sh
@@ -3,6 +3,6 @@
 VERSION=$(git rev-parse HEAD)
 
 yarn run sentry-cli releases -p $SENTRY_PROJECT new $VERSION && \
-  yarn run sentry-cli releases -p $SENTRY_PROJECT set-commits --auto $VERSION && \
-  yarn run sentry-cli releases -p $SENTRY_PROJECT files $VERSION upload-sourcemaps --url-prefix https://$TARGET_HOSTNAME/static/js build/static/js && \
+  yarn run sentry-cli releases -p $SENTRY_PROJECT set-commits --auto $VERSION
+yarn run sentry-cli releases -p $SENTRY_PROJECT files $VERSION upload-sourcemaps --url-prefix https://$TARGET_HOSTNAME/static/js build/static/js && \
   yarn run sentry-cli releases -p $SENTRY_PROJECT deploys $VERSION new --env $TARGET_HOSTNAME
