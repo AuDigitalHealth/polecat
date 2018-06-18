87d4a1eb43ef0ed2e8612e8c2dee7250b42689c3
John Grimes <john@grimes.id.au>
2018-06-18 16:26:26 +1000

    Fix syntax errors with CircleCI config

---
+++ scripts/validateCircleCIConfig.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script taken from: https://circleci.com/blog/circleci-hacks-validate-circleci-config-on-every-commit-with-a-git-hook/
+
+# The following line is needed by the CircleCI Local Build Tool (due to Docker interactivity)
+exec < /dev/tty
+
+# If validation fails, tell Git to stop and provide error message. Otherwise, continue.
+if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
+	echo "CircleCI Configuration Failed Validation."
+	echo $eMSG
+	exit 1
+fi
